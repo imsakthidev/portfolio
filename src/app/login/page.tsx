43d17f3d9fbc5d8e814b51e42c7e1acc87cb8c89
sakthi <imsakthidev@gmail.com>
2026-09-05 14:46:37 +0530

    Fix signInWithPopup import error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import { 
-  OAuthProvider, 
+  OAuthProvider, signInWithPopup,
   signInWithEmailAndPassword, createUserWithEmailAndPassword,
   RecaptchaVerifier, signInWithPhoneNumber
 } from 'firebase/auth';
